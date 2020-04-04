d8e61005c1ee9ebd0cc56a61ca7aa10e8f878c75
Panda-weiyang <63085145+Panda-weiyang@users.noreply.github.com>
2020-04-04 18:25:37 +0800

    提交测试

--- panda_dubbo_springboot_provider/src/main/java/cn/panda/dubbo/springboot/provider/bootstrap/PandaDubboSpringbootProviderBootstrap.java
+++ panda_dubbo_springboot_provider/src/main/java/cn/panda/dubbo/springboot/provider/bootstrap/PandaDubboSpringbootProviderBootstrap.java
@@ -1,10 +1,14 @@
 package cn.panda.dubbo.springboot.provider.bootstrap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 
 @EnableAutoConfiguration
 public class PandaDubboSpringbootProviderBootstrap {
+
+    private static final Logger logger = LoggerFactory.getLogger(PandaDubboSpringbootProviderBootstrap.class.getName());
 
     public static void main(String[] args) {
         SpringApplication.run(PandaDubboSpringbootProviderBootstrap.class, args);
